fba27a140f29f863c8eec3c4da7b8480d9fafbe1
Kelefouras Vasilis <v.kelefouras@leeds.ac.uk>
2017-11-09 09:58:32 +0000

    Update main.m

--- main.m
+++ main.m
@@ -1,4 +1,3 @@
-%the more the fast nodes are AND the fastest the fast nodes are, the more the tasks can be issued without saving any cores
 
 %clear
 clearvars -except R1 R2 R3 R4 R5 R6 R7 R8 R9 R10 R11 R12
